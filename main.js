3ce5aef1cae790d960e36788a7a49f61cab40579
b-iurea <b.iurea94@gmail.com>
2025-06-29 22:43:27 +0200

    bella

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 300,
+    height: 450,
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
